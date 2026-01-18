6fc39e5128d98e5319a3ea85cfd5da3ac498f3c9
Nischal Shrestha <sthanischal5060@gmail.com>
2026-01-18 23:42:14 +0545

    2 way binding

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const App = () => {
+  const [val, setVal] = useState("")
   return (
-    <div>
-      
+
+    <div className=' h-screen w-full flex justify-center items-center'>
+      <div >
+        <input value={val} onChange={(e) => {
+          setVal(e.target.value);
+
+        }}
+          className=' h-10 border-2' type="text" name="" id="" />
+        <button className=' border rounded-2xl px-6 py-1 bg-blue-500'>Submit</button>
+        <button
+          onClick={(e) => {
+            console.log("geef");
+            setVal("")
+          }} className=' border rounded-2xl px-6 py-1 bg-blue-500'>reset</button>
+      </div>
     </div>
   )
 }
